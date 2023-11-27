503c9a409a5cca698ee2fb51623a827869d38226
ehoang0106 <khoahoang1698@gmail.com>
2023-11-26 23:49:15 -0800

    new project using React

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>Hard-working Python Blog</h1>
+            <div className="link">
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
+            </div>
+        </nav>
+
+
+    );
+}
+ 
+export default Navbar;
